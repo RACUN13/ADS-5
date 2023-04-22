20b67ae266c346b888e56f66f019dc9a0d0ce72f
RACUN13 <125272048+RACUN13@users.noreply.github.com>
2023-04-22 21:13:48 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,75 +3,106 @@
 #include <map>
 #include "tstack.h"
 
-int prior(char ch) {
-    if (ch == ')' || ch == '(')
-        return 0;
-    if (ch == '+' || ch == '-')
-        return 1;
-    if (ch == '*' || ch == '/')
-        return 2;
-    else
-        return 0;
+int priorGet(char symbol) {
+  switch (symbol) {
+  case '(':
+    return 0;
+  case ')':
+    return 1;
+  case '+':
+    return 2;
+  case '-':
+    return 2;
+  case '*':
+    return 3;
+  case '/':
+    return 3;
+  case ' ':
+    return -100;
+  }
+  return -1;
 }
 
 std::string infx2pstfx(std::string inf) {
-  TStack<char, 100> stack1;
-  std::string s = "";
-  for (int i = 0; i < inf.length(); i++) {
-    if ((inf[i] >= '0') && (inf[i] <= '9')) {
-      s += inf[i];
-      s += " ";
-    } else if (inf[i] == '(') {
-      stack1.push(inf[i]);
-    } else if (isOperand(inf[i])) {
-        while (!stack1.isEmpty() &&
-               getPrior(inf[i]) <= getPrior(stack1.getValue())) {
-            s += stack1.getValue();
-            s += " ";
-            stack1.pop();
-        }
-        stack1.push(inf[i]);
-    } else if (inf[i] == ')') {
-        while (!stack1.isEmpty() && stack1.getValue() != '(') {
-        s += stack1.getValue();
-        s += " ";
-        stack1.pop();
+  TStack<char, 100> stack;
+  std::string output = "";
+  int prior = 0;
+  int gratPrior = -1;
+  for (auto& op : inf) {
+    prior = priorGet(op);
+    gratPrior = priorGet(stack.get());
+    if (op == '\n') {
+      while (priorGet(stack.get()) > 1) {
+        output += stack.pop();
+        output += " ";
       }
-      stack1.pop();
+      stack.pop();
+    }
+    if (prior == -1) {
+      output += op;
+      output += " ";
+    } else if (prior == 0) {
+      stack.push(op);
+    } else if (prior == 1) {
+      while (priorGet(stack.get()) > 0) {
+        output += stack.pop();
+        output += " ";
+      }
+      stack.pop();
+    } else if (prior > gratPrior || stack.isEmpty()) {
+      stack.push(op);
+    } else if (prior <= gratPrior && prior > 1) {
+      while (priorGet(stack.get()) > 1) {
+        output += stack.pop();
+        output += " ";
+      }
+      stack.push(op);
     }
   }
-  while (!stack1.isEmpty()) {
-    s += stack1.getValue();
-    s += " ";
-    stack1.pop();
+  while (priorGet(stack.get()) > 1) {
+    output += stack.pop();
+    output += " ";
   }
-  s.pop_back();
-  return s;
+  std::string out = "";
+  for (int i = 0; i < output.length() - 1; ++i) {
+    out += output[i];
+  }
+  return out;
 }
-
+int opreoretat(int a, int b, char op) {
+  switch (op) {
+  case '+':
+    return a + b;
+  case '-':
+    return a - b;
+  case '*':
+    return a * b;
+  case '/':
+    return a / b;
+  }
+  return 0;
+}
 int eval(std::string pref) {
-  TStack<int, 100> stack2;
-  int k, l, g;
-  for (int i = 0; i < pref.length(); i++) {
-    if (pref[i] == ' ') {
+  TStack<int, 100> stack1;
+  int preoret = 0;
+  int prior = 0;
+  for (auto& op : pref) {
+    prior = priorGet(op);
+    if (prior == -100) {
       continue;
-    } else if (pref[i] >= '0' && pref[i] <= '9') {
-      k = pref[i] - '0';
-      stack2.push(k);
-    } else if (pref[i] == '+' || pref[i] == '-' ||
-          pref[i] == '*' || pref[i] == '/') {
-      l = stack2.getValue();
-      stack2.pop();
-      g = stack2.getValue();
-      stack2.pop();
-      if (pref[i] == '+') stack2.push(g + l);
-      else if (pref[i] == '-')
-        stack2.push(g - l);
-      else if (pref[i] == '*')
-        stack2.push(g * l);
-      else if (pref[i] == '/')
-        stack2.push(g / l);
+    }
+    if (prior == -1) {
+      stack1.push(op - 48);
+    } else {
+      int peremen = 0;
+      while (!(peremen + stack1.isEmpty())) {
+        peremen += 1;
+        int a = stack1.pop();
+        int b = stack1.pop();
+        preoret = opreoretat(b, a, op);
+        stack1.push(preoret);
+      }
     }
   }
-  return stack2.getValue();
+  return stack1.pop();
 }
